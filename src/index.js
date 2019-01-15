0558b463d7ca998dbf554b1381e55e95058045cd
Josie <nagy.iozefina@yahoo.com>
2019-01-15 19:35:58 +0100

    Create Card UI as a container for content
    Add {props.children} to insert <CommentDetail> component as a child prop

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 4:00PM" 
-        comment="Looking great!"
-        avatar={faker.image.avatar()} 
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:00PM" 
+          comment="Looking great!"
+          avatar={faker.image.avatar()} 
       />
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Today at 6:45PM" 
-        comment="Looks like it still needs a bit of work."
-        avatar={faker.image.avatar()} 
-      />
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Yesterday at 1:16AM" 
-        comment="Would recommend."
-        avatar={faker.image.avatar()} 
-      />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Today at 6:45PM" 
+          comment="Looks like it still needs a bit of work."
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Yesterday at 1:16AM" 
+          comment="Would recommend."
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
     </div>
   );
 };
