940fc8374cda361780e220138245d7990b29f4ec
Josie <nagy.iozefina@yahoo.com>
2019-01-23 18:32:47 +0200

    Add map() function retrieve data from commentData.json

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import faker from 'faker';
+import commentData from './commentData';
 
 const CommentDetail = props => {
+  const test = commentInfo.map(comment => {
+    return (
+      <div>
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img alt="avatar" src={props.comment.avatar} />
+          </a>
+        </div>
+        <div className="content">
+          <a href="/" className="author">
+            {props.comment.author}
+          </a>
+          <div className="date-metadata">
+            <span className="date">{props.comment.timeAgo}</span>
+          </div>
+          <div className="text">{props.comment.message}</div>
+        </div>
+      </div>
+    )
+  })
+
+
   return (
     <div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={props.avatar} />
-        </a>
-      </div>
-      <div className="content">
-        <a href="/" className="author">
-          {props.author}
-        </a>
-        <div className="date-metadata">
-          <span className="date">{props.timeAgo}</span>
-        </div>
-        <div className="text">{props.comment}</div>
-      </div>
+     {test}
     </div>
   );
 };
