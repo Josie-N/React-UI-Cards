f22f2cf76bafc7522b8d585051105ef3645a1664
Steven Boutcher <sjboutcher@gmail.com>
2019-01-26 12:23:41 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { commentInfo } from "./commentData.json"
 import App from "./App";
 
 ReactDOM.render(
-  <App />,
+  <App commentData = {commentInfo}/>,
   document.querySelector('#root')
 );
